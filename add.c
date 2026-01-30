7b3422ab61ab48aeb05ba5c33f77b85e84c51b57
Franklinx62 <ahmedmhmoad888@gmail.com>
2026-01-30 18:54:42 +0200

    Add files via upload
    simple code to addation

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    // عايزين نعمل الة حاسبة
+    int x =0;
+    int z =0;
+    printf("enter the first number:");
+    scanf("%d",&x);
+    printf("enter the second number:");
+    scanf("%d",&z);
+    printf("addation is :%d\n",x+z);
+    return 0;
+}
